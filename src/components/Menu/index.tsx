59a164d6632aecbda2fb93a5e2629238b7c668be
Marcos <devmarcos7@gmail.com>
2025-03-23 09:30:33 -0300

    criando componente

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,12 @@
+function Menu(){
+    return(
+        <>
+            <h1>Marcos Renan</h1>
+            <h2>Projetos</h2>
+            <h2>Sobre</h2>
+            <h2>Contato</h2>
+        </>
+    )
+}
+
+export default Menu;
